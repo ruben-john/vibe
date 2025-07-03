5355360a1a1fc6050c442a913f9dc4a0e8c8579d
Ruben John <rubensajeev@gmail.com>
2025-07-03 18:09:35 +0530

    04:background-jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,12 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    //imagine this is downlaod
+    await step.sleep("wait-a-moment", "10s");
+    await step.sleep("wait-a-moment", "5s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);

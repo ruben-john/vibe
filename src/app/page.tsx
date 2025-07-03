1d189faf9ae2051886a4f8e3f35147897c26f995
Ruben John <rubensajeev@gmail.com>
2025-07-03 19:18:52 +0530

    05: ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useTRPC } from '@/trpc/client';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
 const Page= ()=>{
+    const [value,setValue]=useState("");
     const trpc =useTRPC();
     const invoke=useMutation(trpc.invoke.mutationOptions({
         onSuccess:()=>{
@@ -14,7 +17,8 @@ const Page= ()=>{
     }))
     return(
     <div className="p-4 max-w-7xl mx-auto">
-        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text:"ruben"})}>
+        <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value:value})}>
             Invoke Background job
         </Button>
     </div>
